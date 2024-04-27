ce147aeb8d221a0d42c83d2bbdc7a09630c37a67
imenchac <imenchac@cisco.com>
2024-04-26 20:36:15 -0600

    ST7735

--- GPIO/source/GPIO.c
+++ GPIO/source/GPIO.c
@@ -1,25 +1,28 @@
-/* p4_2.c UART0 Receive
-* Receiving any key from terminal emulator (TeraTerm) of the
-* host PC to the UART0 on Freescale FRDM-KL25Z board.
-* UART0 is connected to openSDA debug agent and has
-* a virtual connection to the host PC COM port.
-* Launch TeraTerm on a PC and hit any key.
-* The LED program from P2_7 of Chapter 2 is used to turn
-* on the tri-color LEDs according to the key received.
-*
-* By default in SystemInit(), FLL clock output is 41.94 MHz.
-* Setting BDH=0, BDL=0x17, and OSR=0x0F yields 115200 Baud.
-*/
 #include <MKL25Z4.H>
+#include <string.h>
+#include "st7735.h"
+#include "fonts.h"
+#include "testimg.h"
+
+/*P8_1.c: Send ‘A’ to ‘Z’ via SPI0
+* PTD1 pin as SPI SCK
+* PTD2 pin as SPI MOSI
+* PTD0 pin as SPI SS
+*/
+
+void GPIO_init(void);
 void UART0_init(void);
+void SPI0_init(void);
 void delayMs(int n);
-void LED_init(void);
 void LED_set(int value);
+void SPI0_write(unsigned char* data, int size);
 
 int main (void) {
+	unsigned char h;
 	char c;
+	SPI0_init();                 /* enable SPI0 */
 	UART0_init();
-	LED_init();
+	GPIO_init();
 	while (1) {
 		while(!(UART0->S1 & 0x20)) {}   /* wait for receive buffer full */
 		c = UART0->D; /* read the char received */
@@ -40,32 +43,57 @@ void UART0_init(void) {
 	PORTA->PCR[1] = 0x0200; /* make PTA1 UART0_Rx pin */
 }
   /* initialize all three LEDs on the FRDM board */
-void LED_init(void){
+void GPIO_init(void){
 	SIM->SCGC5 |= 0x400;        /* enable clock to Port B */
-	SIM->SCGC5 |= 0x1000;       /* enable clock to Port D */
 
 	PORTB->PCR[18] = 0x100;     /* make PTB18 pin as GPIO */
 	PTB->PDDR |= 0x40000;       /* make PTB18 as output pin */
 	PTB->PSOR = 0x40000;        /* turn off red LED */
-
+	PORTB->PCR[8] = 0x100;     /* make PTB8 pin as GPIO CS*/
+	PTB->PDDR |= 0x80;       /* make PTB8 as output pin */
+	PTB->PSOR = 0x80;        /* turn off pin */
+	PORTB->PCR[9] = 0x100;     /* make PTB9 pin as GPIO REST*/
+	PTB->PDDR |= 0x100;       /* make PTB9 as output pin */
+	PTB->PSOR = 0x100;        /* turn off pin */
+	PORTB->PCR[10] = 0x100;     /* make PTB10 pin as GPIO DS*/
+	PTB->PDDR |= 0x200;       /* make PTB10 as output pin */
+	PTB->PSOR = 0x200;        /* turn off pin */
 	PORTB->PCR[19] = 0x100;     /* make PTB19 pin as GPIO */
 	PTB->PDDR |= 0x80000;       /* make PTB19 as output pin */
 	PTB->PSOR = 0x80000;        /* turn off green LED */
-
-	PORTD->PCR[1] = 0x100;      /* make PTD1 pin as GPIO */
-	PTD->PDDR |= 0x02;          /* make PTD1 as output pin */
-	PTD->PSOR = 0x02;           /* turn off blue LED */
+}
+  /* initialize SPIO */
+void SPI0_init(void) {
+	SIM->SCGC5 |= 0x1000;       /* enable clock to Port D */
+	PORTD->PCR[1] = 0x200;      /* make PTD1 pin as SPI SCK */
+	PORTD->PCR[2] = 0x200;      /* make PTD2 pin as SPI MOSI */
+	PORTD->PCR[0] = 0x100;      /* make PTD0 pin as GPIO */
+	PTD->PDDR |= 0x01;          /* make PTD0 as output pin for /SS */
+	PTD->PSOR = 0x01;           /* make PTD0 idle high */
+	SIM->SCGC4 |= 0x400000;     /* enable clock to SPI0 */
+	SPI0->C1 = 0x10;            /* disable SPI and make SPI0 master */
+	SPI0->BR = 0x60;            /* set Baud rate to 1 MHz */
+	SPI0->C1 |= 0x40;           /* Enable SPI module */
 }
 /* turn on or off the LEDs according to bit 2-0 of the value */
 void LED_set(int value){
 	if (value != '\n'){
 		if (value & 1){   			/* use bit 0 of value to control red LED */
 			PTB->PCOR = 0xC0000;    /* turn on red LED */
-			PTD->PCOR = 0x02;		/* turn on blue LED */
 		}
 		else {
 			PTB->PSOR = 0xC0000;    /* turn off red LED */
-			PTD->PSOR = 0x02;       /* turn off blue LED */
 		}
+	}
+}
+/* write a character to SPI0 */
+void SPI0_write(unsigned char* data, int size) {
+	volatile char dummy;
+	for (size_t i = 0; i < size; i++)
+	{
+		while(!(SPI0->S & 0x20)) { }    /* wait until tx ready */
+		SPI0->D = *(data + i);                 /* send register address */
+		while(!(SPI0->S & 0x80)) { }    /* wait until tx complete */
+		dummy = SPI0->D;                /* clear SPRF */
 	}
 }
